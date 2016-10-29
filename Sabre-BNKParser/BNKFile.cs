5165e685035bdecb5145090e485afab284144aa4
Crauzer <thenatokenproject@gmail.com>
2016-10-29 11:26:38 +0200

    Add project files.

---
+++ Sabre-BNKParser/BNKFile.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sabre_BNKParser
+{
+    class BNKFile
+    {
+        public BinaryReader br;
+        public BKHD bkhd;
+        public DIDX didx;
+        public DATA data;
+        public string fileLoc;
+        public BNKFile(string fileLocation, bool isAudioBank = false, bool isEventBank = false, bool isChampionBank = false, bool extractAudio = false)
+        {
+            fileLoc = fileLocation;
+            br = new BinaryReader(File.Open(fileLocation, FileMode.Open));
+            if(isAudioBank == true)
+            {
+                bkhd = new BKHD(br);
+                if(isChampionBank == true)
+                {
+                    br.ReadBytes(12);
+                }
+                didx = new DIDX(br);
+                data = new DATA(br, didx, bkhd);
+                if(extractAudio == true)
+                {
+                    foreach(var a in didx.Files)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Extracting " + a.ID.ToString() + ".wem");
+                        DIDXEmbeddedWEMFile.ExportAudio(a.ID.ToString() + ".wem", a.Data, this);
+                    }
+                }
+            }
+            else if(isEventBank == true)
+            {
+                bkhd = new BKHD(br);
+            }
+        }
+        public class BKHD //28
+        {
+            public string Magic;
+            public UInt32 LengthOfSection;
+            public UInt32 Version;
+            public UInt32 ID;
+            public UInt32[] Zero = new UInt32[3];
+            public BKHD(BinaryReader br)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                LengthOfSection = br.ReadUInt32();
+                Version = br.ReadUInt32();
+                ID = br.ReadUInt32();
+                for(int i = 0; i < 3; i++)
+                {
+                    Zero[i] = br.ReadUInt32();
+                }
+            }
+        }
+        public class DIDX // 12 + 12 * WEM
+        {
+            public string Magic;
+            public UInt32 Length;
+            public UInt32 WEMCount;
+            public List<DIDXEmbeddedWEMFile> Files = new List<DIDXEmbeddedWEMFile>();
+            public DIDX(BinaryReader br)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                Length = br.ReadUInt32();
+                WEMCount = Length / 12;
+                for(int i = 0; i < WEMCount; i++)
+                {
+                    Files.Add(new DIDXEmbeddedWEMFile(br));
+                }
+            }
+        }
+        public class DATA
+        {
+            public string Magic;
+            public UInt32 Length;
+            public DATA(BinaryReader br, DIDX didx, BKHD bkhd)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                Length = br.ReadUInt32();
+                foreach(var f in didx.Files)
+                {
+                    br.BaseStream.Seek(CalculateOffsetBNK(didx.WEMCount) + f.OffsetDataSection, SeekOrigin.Begin);
+                    f.Data = br.ReadBytes((int)f.DataLength);
+                }
+            }
+        }
+        public class HIRC
+        {
+            public string Magic;
+            public UInt32 Length;
+            public UInt32 NumberOfEvents;
+            public HIRC(BinaryReader br)
+            {
+
+            }
+        }
+        public class STID
+        {
+            public string Magic;
+            public UInt32 Length;
+            public UInt32 ZeroOne;
+            public UInt32 SoundbankCount;
+            public List<STIDSoundbank> Soundbanks = new List<STIDSoundbank>();
+            public STID(BinaryReader br)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                Length = br.ReadUInt32();
+                ZeroOne = br.ReadUInt32();
+                SoundbankCount = br.ReadUInt32();
+                for(int i = 0; i < SoundbankCount; i++)
+                {
+                    Soundbanks.Add(new STIDSoundbank(br));
+                }
+            }
+        }
+        public class STMG
+        {
+            public STMG(BinaryReader br)
+            {
+
+            }
+        }
+        public class STIDSoundbank
+        {
+            public UInt32 ID;
+            public byte LengthOfBank;
+            public string Bank;
+            public STIDSoundbank(BinaryReader br)
+            {
+                ID = br.ReadUInt32();
+                LengthOfBank = br.ReadByte();
+                Bank = Encoding.ASCII.GetString(br.ReadBytes(LengthOfBank));
+            }
+        }
+        public class DIDXEmbeddedWEMFile
+        {
+            public UInt32 ID;
+            public UInt32 OffsetDataSection;
+            public UInt32 DataLength;
+            public byte[] Data;
+            public DIDXEmbeddedWEMFile(BinaryReader br)
+            {
+                ID = br.ReadUInt32();
+                OffsetDataSection = br.ReadUInt32();
+                DataLength = br.ReadUInt32();
+            }
+            public static void ExportAudio(string fileName, byte[] data, BNKFile bnk)
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + "/OUTPUT/" + "/" + bnk.fileLoc + "/");
+                File.WriteAllBytes(Environment.CurrentDirectory + "/OUTPUT/" + "/" + bnk.fileLoc + "/" + fileName, data);
+            }
+        }
+        public static long CalculateOffsetBNK(uint NumberOfEmbeddedSoundbanks)
+        {
+            long offset = 56;
+            offset += NumberOfEmbeddedSoundbanks * 12;
+            return offset;
+        }   
+    }
+}
---
+++ Sabre-BNKParser/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabre_BNKParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool extract = false;
+            bool isChampionBank = false;
+            bool isEventBank = false;
+            bool isAudioBank = false;
+            Console.WriteLine("Example: Lucian_Skin06_SFX_audio.bnk -E -CHAMPBANK -AUDIOBANK      -E - extract, -CHAMPBANK - Champion bank, -EVENTBANK");
+            string input = Console.ReadLine();
+            string[] inp = input.Split(' ');
+            if(inp.Length == 1)
+            {
+                inp = new string[] {inp[0], "", "", "" };
+            }
+            if(inp.Length == 2)
+            {
+                inp = new string[] {inp[0], inp[1], "", "" };
+            }
+            if(inp.Length == 3)
+            {
+                inp = new string[] {inp[0], inp[1], inp[2], "" };
+            }
+            if(inp[1] == "-E" || inp[2] == "-E" || inp[3] == "-E")
+            {
+                extract = true;
+            }
+            if(inp[1] == "-CHAMPBANK" || inp[2] == "-CHAMPBANK" || inp[3] == "-CHAMPBANK")
+            {
+                isChampionBank = true;
+            }
+            if(inp[1] == "-AUDIOBANK" || inp[2] == "-AUDIOBANK" || inp[3] == "-AUDIOBANK")
+            {
+                isAudioBank = true;
+            }
+            if(inp[1] == "-EVENTBANK" || inp[2] == "-EVENTBANK" || inp[3] == "-EVENTBANK")
+            {
+                isEventBank = true;
+            }
+            if(isEventBank == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Event banks not supported");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+            else
+            {
+                BNKFile b = new BNKFile(inp[0], isAudioBank, isEventBank, isChampionBank, extract);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("------BKHD------");
+                Console.ResetColor();
+                Console.WriteLine("Magic = " + b.bkhd.Magic);
+                Console.WriteLine("Length = " + b.bkhd.LengthOfSection);
+                Console.WriteLine("ID = " + b.bkhd.ID);
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("------DIDX------");
+                Console.ResetColor();
+                Console.WriteLine("Magic = " + b.didx.Magic);
+                Console.WriteLine("Length = " + b.didx.Length);
+                Console.WriteLine("WEMCount = " + b.didx.WEMCount);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("------EMBEDDED WEM FILES------");
+                Console.ResetColor();
+                foreach(var f in b.didx.Files)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ID = " + f.ID);
+                    Console.WriteLine("Offset from DATA section = " + f.OffsetDataSection);
+                    Console.WriteLine("Data Length = " + f.DataLength);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("------DATA------");
+                Console.ResetColor();
+                Console.WriteLine("Magic = " + b.data.Magic);
+                Console.WriteLine("Length = " + b.data.Length);
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+        public static long CalculateOffsetBNK(uint NumberOfEmbeddedSoundbanks)
+        {
+            long offset = 56;
+            offset += NumberOfEmbeddedSoundbanks * 12;
+            return offset;
+        }   
+    }
+}
